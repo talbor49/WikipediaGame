3f780ba0a2a9ea78ec908328079a91950c5f4d5d
talbor49 <talbor49@gmail.com>
2016-05-30 01:21:06 -0500

    Random article is now from top 5000, and added replay button

---
+++ app/src/main/java/com/example/tal/wikipediagame/Article.java
@@ -0,0 +1,55 @@
+package com.example.tal.wikipediagame;
+
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Article {
+
+    private int pageid;
+    private String name;
+    private String url;
+
+
+    public Article() {}
+
+    public Article(int pageid, String name) {
+        this.pageid = pageid;
+        this.name = name;
+    }
+
+    public Article(int pageid, String name, String url) {
+        this.pageid = pageid;
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getPageid() {
+        return pageid;
+    }
+
+    public void setPageid(int pageid) {
+        this.pageid = pageid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void generateName() {
+
+
+        this.name = "Donland Trump";
+    }
+}
--- app/src/main/java/com/example/tal/wikipediagame/PlayMode.java
+++ app/src/main/java/com/example/tal/wikipediagame/PlayMode.java
@@ -1,27 +1,36 @@
 package com.example.tal.wikipediagame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class PlayMode extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
 
-    static WebView wikipediaWv;
-    static TextView destinationTextView;
-    static Button butt;
-    static TextView youWinTv;
-    static String destinationUrl;
-    static String destionationName;
-    static Button playAgainButton;
-    static int pagesCount;
+public class PlayMode extends Activity {
+
+    private Article destinationArticle;
+    private WebView wikipediaWv;
+    private TextView destinationTextView;
+    private TextView youWinTv;
+    private String tempDestUrl;
+    private Button playAgainButton;
+    private ImageButton replayTopButton;
+    private WebView utilityWebView;
+    private int pagesCount;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        destinationArticle  = new Article();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_mode);
         wikipediaWv = (WebView) findViewById(R.id.wikipediaWebView);
@@ -38,45 +47,38 @@ public class PlayMode extends AppCompatActivity {
         playAgainButton = (Button) findViewById(R.id.playAgainButton);
         assert playAgainButton != null;
 
-        final String randomArticle = "https://en.wikipedia.org/wiki/Special:Random";
+        replayTopButton = (ImageButton) findViewById(R.id.replayButton);
+        assert replayTopButton != null;
 
 
 
-
-
-
-
-
-        final WebView utilityWebView = new WebView(getApplicationContext());
+        utilityWebView = new WebView(getApplicationContext());
         WebViewClient utilityWebViewClient = new WebViewClient() {
             @Override
             public void onLoadResource(WebView view, String url) {
                 super.onLoadResource(view, url);
-                if (!url.equals(randomArticle)) {
-                    destinationUrl = view.getUrl();
-                    destionationName = destinationUrl.substring(destinationUrl.indexOf("wiki/") + "wiki/".length() + 1);
-                    destinationTextView.setText("Destination: " + destionationName);
+                if (!url.equals(tempDestUrl)) {
+                    destinationArticle.setUrl(view.getUrl());
+                    destinationArticle.generateName();
+                    String descriptionText = "Destination article title: " + destinationArticle.getName();
+                    destinationTextView.setText(descriptionText);
                 }
             }
         };
         utilityWebView.setWebViewClient(utilityWebViewClient);
-        utilityWebView.loadUrl(randomArticle);
 
 
         //Initialize and set the play again button
         playAgainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get a new random destination article
-                utilityWebView.loadUrl(randomArticle);
-
-                // Load a new starting point article
-                wikipediaWv.loadUrl(randomArticle);
-                wikipediaWv.setVisibility(View.VISIBLE);
-                playAgainButton.setVisibility(View.INVISIBLE);
-                youWinTv.setVisibility(View.INVISIBLE);
-
-                pagesCount = 0;
+                startNewGame();
+            }
+        });
+        replayTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewGame();
             }
         });
 
@@ -86,7 +88,7 @@ public class PlayMode extends AppCompatActivity {
             @Override
             public void onLoadResource(WebView view, String url) {
                 super.onLoadResource(view, url);
-                if (url.equals(destinationUrl)) {
+                if (url.equals(destinationArticle.getUrl())) {
                     view.setVisibility(View.INVISIBLE);
                     String victoryText = "You win! Great job! it only took you " + pagesCount + " moves!";
                     youWinTv.setText(victoryText);
@@ -100,7 +102,25 @@ public class PlayMode extends AppCompatActivity {
             }
         });
         
-        wikipediaWv.loadUrl("https://en.wikipedia.org/wiki/Special:Random");
 
+        startNewGame();
+    }
+
+
+    private void startNewGame() {
+        // Get a new random destination article. sets automatically
+        destinationArticle.setPageid(ArticleUtils.getRandomArticleId());
+        tempDestUrl = "http://en.wikipedia.org/?curid=" + destinationArticle.getPageid();
+        utilityWebView.loadUrl(tempDestUrl);
+
+        // Load a new starting point article
+        wikipediaWv.loadUrl("http://en.m.wikipedia.org/?curid=" + ArticleUtils.getRandomArticleId());
+
+        //Hide and show relevant views
+        wikipediaWv.setVisibility(View.VISIBLE);
+        playAgainButton.setVisibility(View.INVISIBLE);
+        youWinTv.setVisibility(View.INVISIBLE);
+
+        pagesCount = 0;
     }
 }
