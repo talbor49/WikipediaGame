1e03388156b3d72f2d41430e253b927e8af098ba
Tal Bortman <talbor49@gmail.com>
2016-05-30 23:40:44 +0300

    Added starting screen

---
+++ app/src/main/java/com/example/tal/wikipediagame/StartingScreen.java
@@ -0,0 +1,28 @@
+package com.example.tal.wikipediagame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class StartingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void startNewSinglePlayerGame(View singlePlayerButton) {
+        Intent intent = new Intent(this, PlayMode.class);
+        intent.putExtra("multiplayer", false);
+        startActivity(intent);
+    }
+
+}
